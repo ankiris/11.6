9946109fee7a32786238451a14750f2a93d1c4e5
Kat Lasota <lasota.kat@gmail.com>
2018-07-26 13:49:53 +0200

    change url in js

--- js/App.js
+++ js/App.js
@@ -9,7 +9,7 @@ function generateTemplate(name, data, basicElement) {
 	return element;
 }
 
-var baseUrl = 'https://kodilla.com/pl/bootcamp-api/';
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
   'X-Client-Id': 'X-Client-Id',
   'X-Auth-Token': 'X-Auth-Token',
